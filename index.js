9205d94878c642cfdef6bfc8f5487ee88d6cc0b9
Jeremy Jalbuena <jalbuena_jeremy@yahoo.com>
2022-12-19 14:18:14 +0800

    commit tree df7b5095824f587ee72237c158e8f019917ba2be

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+const wednesdayMenu = {
+  cheesePlate: {
+    soft: "Brie",
+    semiSoft: "Fontina",
+    hard: "Provolone",
+  },
+  fries: "Sweet potato",
+  salad: "Southwestern",
+};
+
+Object.keys(wednesdayMenu);
+//=> ["cheesePlate", "fries", "salad"]
